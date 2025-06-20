b098d1dfa6eea5cf079353e8f17d325d7651cc6d
Felipe Parada V <felipeparadavasquez@gmail.com>
2025-06-20 17:39:01 -0400

    feat: mejora inicial en MarketApiApplication

--- src/main/java/com/felipe/Market_api/MarketApiApplication.java
+++ src/main/java/com/felipe/Market_api/MarketApiApplication.java
@@ -1,5 +1,6 @@
 package com.felipe.Market_api;
 
+import com.felipe.Market_api.domain.model.Category;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,6 +11,8 @@ public class MarketApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MarketApiApplication.class, args);
+
 	}
+
 
 }
